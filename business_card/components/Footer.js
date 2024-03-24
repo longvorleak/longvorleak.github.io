83913def91580cea4789b296ff754f385eb5b18e
Vorleak <longvorleak@gmail.com>
2024-03-24 20:42:43 +0900

    clean up project

--- business_card/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer> <p className="footer-copyright">© 2022 Vorleak. All rights reserved.</p>
-        </footer>
-    )
-}
